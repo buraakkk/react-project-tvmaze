14c41991324e13ea8ace3ae1b2996bf8d70560f1
Burak ISIK <50028862+buraakkk@users.noreply.github.com>
2021-06-15 14:17:58 +0200

    Update Footer.js

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,8 +1,4 @@
 import React from "react";
-// import InstagramIcon from "@material-ui/icons/Instagram";
-// import TwitterIcon from "@material-ui/icons/Twitter";
-// import FacebookIcon from "@material-ui/icons/Facebook";
-// import LinkedInIcon from "@material-ui/icons/LinkedIn";
 import "../styles/Footer.css";
 
 function Footer() {
